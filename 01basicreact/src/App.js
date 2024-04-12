3c151916f26b593c7c2a98d4a9ed204570b4b85f
1999chandan <chanvishwakarma@gmail.com>
2024-04-12 12:52:20 +0530

    learning react basics

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai or code react basic aap chandan</h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>chai our code | Chandan vishwakarma </h1>
+    </>
+  )
+}
+
+export default App
